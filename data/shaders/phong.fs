7ebea5c9f0d1e868f4487ce760d032a4399f4630
Alba Torra <60510804+Albuxy@users.noreply.github.com>
2020-10-19 15:36:49 +0200

    phong edited

--- data/shaders/phong.fs
+++ data/shaders/phong.fs
@@ -19,18 +19,19 @@ uniform vec3 u_camera_position;
 void main()
 {
 	vec2 uv = v_uv;
-	vec4 color = u_color
+	vec4 color = u_color;
 	vec3 N = normalize(v_normal);
-	color *= texture(u_texture, uv);
+	color = texture(u_texture, uv);
 
 	//----Phong equation---
-	vec3 light = vec3(0,0);
 
-		//--Ambient--
-		vec3 ambient = u_ambient_light;
+	vec3 light = vec3(0.0);
+	vec3 ambient = vec3(0.0);
+	ambient = u_ambient_light;
 
+	
 		//--Diffuse---
-		vec3 diffuse = vec3(0,0);
+		vec3 diffuse = vec3(0.0);
 		vec3 L;
 
 		//Ligh vector is the same for all pixels
@@ -38,22 +39,14 @@ void main()
 		L = normalize(u_light_position - v_world_position);
 		float NdotL = dot(N,L);
 		NdotL = clamp(NdotL, 0.0, 1.0);
-		diffuse = NdotL * u_light_position;
-
-		//--Specular--
-		vec3 specular = vec3(0.0);
-		vec3 V = normalize(u_camera_position - v_world_position);
-		vec3 R = normalize(reflect(-L, N));
-		float RdotV = dot(V,R);
-		RdotV = clamp(RdotV, 0.0, 1.0);
-		float spec = pow(RdotV, 32);
-		specular = spec * u_light_color;
+		diffuse = NdotL * u_light_color;
 
 
 		//---Total Light----
-		light = ambient + diffuse + specular;
+		light = ambient + diffuse;
+		
 
-		color.xyz *= light;
+	color.xyz *= light;
 
 
 	gl_FragColor = color;
